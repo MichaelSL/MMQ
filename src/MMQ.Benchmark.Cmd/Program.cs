c8409f4ab7908b35ed5a74761fd0137c972734c1
Michael Samorokov <michael.samorokov@gmail.com>
2023-11-03 14:45:56 -0600

    added benchmarks project

---
+++ src/MMQ.Benchmark.Cmd/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using MMQ.Benchmark.Cmd;
+
+Console.WriteLine("Hello, World!");
+var summary = BenchmarkRunner.Run<SendAndReadDataBenchmark>(new DebugBuildConfig());
---
+++ src/MMQ.Benchmark.Cmd/SendAndReadDataBenchmark.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MMQ.Benchmark.Cmd
+{
+    public class SendAndReadDataBenchmark: IDisposable
+    {
+        private readonly IMemoryMappedQueue queue;
+        private readonly IMemoryMappedQueueProducer producer;
+        private readonly IMemoryMappedQueueConsumer consumer;
+        
+        public SendAndReadDataBenchmark()
+        {
+            queue = MemoryMappedQueue.Create(nameof(SendAndReadDataBenchmark));
+            producer = queue.CreateProducer();
+            consumer = queue.CreateConsumer();
+        }
+
+        [Benchmark]
+        public void SendMessage()
+        {
+            producer.Enqueue(new byte[]{1,2,3,4,5,6,7,8});
+        }
+
+        [Benchmark]
+        public void ReadMessage()
+        {
+            _ = consumer.Dequeue();
+        }
+
+        public void Dispose()
+        {
+            producer.Dispose();
+            consumer.Dispose();
+            queue.Dispose();
+        }
+    }
+}
